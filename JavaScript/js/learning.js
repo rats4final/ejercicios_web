ade377004d644df74326b6983ce736ffd706cbaa
rats4final <rats4final@gmail.com>
2023-02-23 03:16:33 -0400

    salidas y conceptos basicos

--- JavaScript/js/learning.js
+++ JavaScript/js/learning.js
@@ -1,3 +1,10 @@
 function inner() {
     document.getElementById('inner').innerHTML = 'usando innerHTML'
 }
+function ventana(){
+    window.alert("Usando window.alert")
+    //aunque no es necesario usar window, tambien sirve solo alert()
+}
+function consola(){
+    console.log("console.log es la mejor de estas opciones")
+}
